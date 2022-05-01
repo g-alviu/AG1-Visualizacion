9580d286eaba058c259fc81e6ad62149c146a9be
g-alviu <gdelatorreabaitua@alumnos.viu.es>
2022-05-01 12:42:24 +0200

    2 files modified

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 // Import stylesheets
 import './style.css';
+
+var data = [{"year": 1991, "name":"alpha", "value": 15},
+{"year": 1992, "name":"alpha", "value": 20},]
+
+//Gráfico de columnas.
+var visualization = d3plus.viz()
+.container("#viz1")
+.data(data)
+.type("bar")
+.id("name")
+.x("year")
+.y("value")
+.draw()
+
+var data2 = "https://raw.githubusercontent.com/raul27868/07MBIG-Visualizacion-Actividades-Guiadas/master/columnas.json"
+
+d3.json(data2, function(data) {
+var visualization = d3plus.viz()
+.container("#viz2")
+.data(data)
+.type('bar')
+.id('name')
+.x('year')
+.y('value')
+.axes({ ticks: 'false' })
+.draw();
+});
